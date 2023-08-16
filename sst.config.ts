ea31640bbbba3f8e93bca7bf26fe9c3b4a356a7f
takecchi <takecchi.kobayashi@gmail.com>
2023-08-16 19:10:36 +0900

    AWSテスト用

---
+++ sst.config.ts
@@ -0,0 +1,19 @@
+import {SSTConfig} from 'sst';
+import {NextjsSite} from 'sst/constructs';
+
+export default {
+    config(_input) {
+        return {
+            name: 'nextjs-priority-routes-test',
+            region: 'ap-northeast-1',
+        };
+    },
+    stacks(app) {
+        app.stack(function Site({stack}) {
+            const site = new NextjsSite(stack, 'site', {});
+            stack.addOutputs({
+                SiteUrl: site.url,
+            });
+        });
+    },
+} satisfies SSTConfig;
