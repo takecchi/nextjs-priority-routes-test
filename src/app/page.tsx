2814bf63794d999ddc8b31983e3e7c2e23a2bcd2
takecchi <takecchi.kobayashi@gmail.com>
2023-08-16 16:39:16 +0900

    検証

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,18 +63,18 @@ export default function Home() {
         </Link>
 
         <Link
-          href="/User1"
+          href="/favicon.ico"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            /User1{' '}
+            /favicon.ico{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            except: `UserName: User1`
+            except: `favicon.ico`
           </p>
         </Link>
 
